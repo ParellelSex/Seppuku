f11d6e667866d63c744986605bc8cfe3d679f520
ParellelSex <88294148+ParellelSex@users.noreply.github.com>
2022-05-24 06:00:34 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,1 +1,28 @@
-print("WIP")
+repeat task.wait() until game:IsLoaded() == true
+
+local user = 'ParellelSex'
+local branch = 'Seppuku'
+local link = 'https://raw.githubusercontent.com/'
+
+local function GetURL(url)
+    local get = '%s/%s/%s/main/%s'
+    return game:HttpGet(string.format(get,link,user,branch,url))
+end
+
+local getasset = getsynasset or getcustomasset
+local requestfunc = syn and syn.request or http and http.request or http_request
+
+local function getfile(file)
+    local get = '%s/%s/%s/main/%s'
+	local req = requestfunc({
+		Url = string.format(get,link,user,branch,file),
+		Method = 'GET'
+	})
+	if req.StatusCode == 200 then
+		return req.Body
+	else
+		return nil
+	end
+end
+
+--Start
